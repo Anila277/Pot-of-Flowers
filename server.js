0a61ed7785fa567304833a7db151a605d483e745
Anila Nisar <raniyasr@gmail.com>
2022-09-03 22:37:59 -0400

    require dependencies

---
+++ server.js
@@ -0,0 +1,28 @@
+// DEPENDENCIES
+const express = require('express');
+const mongosse = require('mongoose');
+const methodOverride = require('method-override');
+const expressSession = require('express-session');
+
+// INITIALIZE THE APP
+const app = express();
+
+// CONFIGURE SETTINGS
+
+// CONNECT TO MONGODB
+
+// ADD MONGO DB CONNECTED AND ERROR EVENT LISTENERS
+
+// MOUNT MIDDLEWARE
+
+// BODY PARSER MIDDLEWARE
+
+// SESSION MIDDLEWARE
+
+// AUTHORIZATION MIDDLEWARE
+
+// HOMEPAGE ROUTE
+
+// TELL THE APP TO LISTEN
+
+
