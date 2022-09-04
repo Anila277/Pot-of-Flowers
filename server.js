0f8c908ad628a0537bac7d53a0dcb0c7d6413551
Anila Nisar <raniyasr@gmail.com>
2022-09-03 22:47:33 -0400

    connect to mongoDB

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongosse = require('mongoose');
 const methodOverride = require('method-override');
 const expressSession = require('express-session');
+const { default: mongoose } = require('mongoose');
 
 // INITIALIZE THE APP
 const app = express();
@@ -15,6 +16,10 @@ const DATABASE_URI = process.env.DATABASE_URI;
 const db = mongoose.connections; // what is this?
 
 // CONNECT TO MONGODB
+mongoose.connect(DATABASE_URI);
+  //add mongoDB connected and error event listeners
+  db.concat('connected', () => console.log('Connect to MongoDB'));
+  db.concat('error', (err) => console.log('MongoDB Error: ' + err.message));
 
 // ADD MONGO DB CONNECTED AND ERROR EVENT LISTENERS
 
